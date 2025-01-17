ab8c3e5ee1624d34fb7d49e45fe3e49e65e99dd9
I-amPrashant <airmax50cent@gmail.com>
2025-01-17 15:08:20 +0545

    nav links for navigation add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+import ContactUs from "./pages/ContactUs";
 import "./App.css";
 
 export default function App() {
   return (
     <>
-      <Header />
       <Router>
+      <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/shop" element={<Shop />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact-us" element={<ContactUs />}></Route>
         </Routes>
       </Router>
     </>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect } from "react"
+import {Link, useParams} from "react-router-dom"
+export default function Header(){
+    const [tab, setTab] = useState('')
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+    const {param}=useParams();
 
-export default function Header() {
+    useEffect(()=>{
+        if(param==='/' || param===undefined){
+            return setTab('home')
+        }
+          setTab(param);
+    }, [param])
+
   return (
-    <div>Header</div>
+    <div className="relative py-7 px-3 sm:px-5 bg-black">
+        <div className="relative max-w-[1250px] mx-auto">
+            <div>
+                <ul className="flex gap-12 items-center text-white">
+                    <li className={`relative navLinks ${tab==='home' && 'active text-gray-300'} py-2 cursor-pointer text-base hover:text-gray-300 duration-150 transition-all ease-linear`} onClick={()=>setTab('home')}><Link to='/'>Home</Link></li>
+                    <li className={`relative navLinks ${tab==='shop' && 'active text-gray-300'} py-2 cursor-pointer text-base hover:text-gray-300 duration-150 transition-all ease-linear`} onClick={()=>setTab('shop')}><Link to='/shop'>Shop</Link></li>
+                    <li className={`relative navLinks ${tab==='about' && 'active text-gray-300'} py-2 cursor-pointer text-base hover:text-gray-300 duration-150 transition-all ease-linear`} onClick={()=>setTab('about')}><Link to='/about'>About</Link></li>
+                    <li className={`relative navLinks ${tab==='contact-us' && 'active text-gray-300'} py-2 cursor-pointer text-base hover:text-gray-300 duration-150 transition-all ease-linear`} onClick={()=>setTab('contact-us')}><Link to='/contact-us'>Contact Us</Link></li>
+                </ul>
+            </div>
+        </div>
+    </div>
   )
 }
