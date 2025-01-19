b2a22f325e444f45e588e683d22db7ec1811faaa
I-amPrashant <airmax50cent@gmail.com>
2025-01-19 20:48:11 +0545

    major feature section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -55,7 +55,7 @@ export default function Home() {
 
 
       {/* testimonial  */}
-      <div className="w-full h-[700px] px-3 py-[20px]  flex  justify-center items-center" id='testimonial'>
+      <div className="w-full  px-3 py-[100px]  flex  justify-center items-center" id='testimonial'>
         <div className="max-w-[800px] text-center items-center flex flex-col text-[#fff]">
             <div className="flex gap-4">
               <span className="text-xl text-yellow-400"><i className="fa fa-star"></i></span>
@@ -66,6 +66,32 @@ export default function Home() {
             </div>
             <h1 className="text-3xl font-semibold mt-5 font-serif italic tracking-wider">"PB is my fashion sanctuary! The curated collection effortlessly blends chic trends with timeless elegance, making every purchase a delightful discovery. The quality of their pieces is unmatched, and I appreciate the brand's commitment to sustainable fashion. What truly sets PB apart is their customer-centric approach."</h1>
             <h2 className=" mt-5 tracking-wider font-thin">Mr. Prashant Bartaula</h2>
+        </div>
+      </div>
+
+      {/* our services  */}
+      <div className="bg-white w-full py-[100px] px-4 mb-[100px]">
+        <div className="max-w-[1400px] mx-auto flex flex-wrap justify-center gap-14 items-center">
+          <div className="flex flex-col items-center gap-4 max-w-[300px] text-center">
+              <div className="text-3xl text-gray-800"><i className="fa fa-truck"></i></div>
+              <h1 className="text-2xl font-medium italic">Free Shipping</h1>
+              <p className="text-sm text-gray-500">Shopping with no extra charges – savor the liberty of complimentary shipping on every order.</p>
+          </div>
+          <div className="flex flex-col items-center gap-4 max-w-[300px] text-center">
+              <div className="text-3xl text-gray-800"><i className="fa fa-lock"></i></div>
+              <h1 className="text-2xl font-medium italic">Secure Payments</h1>
+              <p className="text-sm text-gray-500">Shop with confidence knowing that your transactions are safeguarded.</p>
+          </div>
+          <div className="flex flex-col items-center gap-4 max-w-[300px] text-center">
+              <div className="text-3xl text-gray-800"><i className="fa fa-undo"></i></div>
+              <h1 className="text-2xl font-medium italic">Easy Return </h1>
+              <p className="text-sm text-gray-500">With our hassle-free Easy Returns, changing your mind has never been more convenient.</p>
+          </div>
+          <div className="flex flex-col items-center gap-4 max-w-[300px] text-center">
+              <div className="text-3xl text-gray-800"><i className="fa fa-globe"></i></div>
+              <h1 className="text-2xl font-medium italic">Order Tracking</h1>
+              <p className="text-sm text-gray-500">Stay in the loop with our Order Tracking feature – from checkout to your doorstep.</p>
+          </div>
         </div>
       </div>
       </div>
