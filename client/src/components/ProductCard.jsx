7a1bdf1034826931e0601dc280dc84dbc2a1653f
I-amPrashant <airmax50cent@gmail.com>
2025-01-23 17:29:55 +0545

    shop page add

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -6,7 +6,7 @@ export default function ProductCard() {
     <img
       src="https://plus.unsplash.com/premium_photo-1734543932716-431337d9c3c4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8"
       alt="image"
-      className="object-cover h-[300px] w-[270px] sm:w-[300px]"
+      className="object-cover h-[300px] w-[270px]"
     />
       
       <h2 className="text-gray-400 text-sm font-medium mt-5">Category</h2>
--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -1,7 +1,50 @@
 import React from 'react'
+import ProductCard from '../components/ProductCard'
 
 export default function Shop() {
   return (
-    <div>Shop</div>
+    <div className='relative'>
+    <div className='h-[500px] flex justify-center items-center'>
+        <h1 className='text-4xl sm:text-6xl italic font-serif  text-white '>Shop</h1>
+    </div>
+   
+   <div className='bg-white py-[100px] px-4'>
+    <div className='max-w-[1200px] mx-auto relative'>
+      <div className='flex flex-wrap gap-10 items-center justify-between'>
+        <h1 className='text-base text-gray-400'>Showing all results</h1>
+        <div>
+    <label htmlFor="sorting">Sort by: </label>
+        <select name="sorting" className='ml-3 p-1 border cursor-pointer border-gray-600'>
+          <option value="">Default</option>
+          <option value="pasc">Price low to high</option>
+          <option value="pdsc">Price high to low</option>
+          <option value="asc">Popularity</option>
+        </select>
+        </div>
+        <div>
+      <label htmlFor="category">Category: </label>
+        <select name="category" className='ml-2 p-1 border cursor-pointer border-gray-600'>
+          <option value="">Default</option>
+          <option value="mens">Mens</option>
+          <option value="womens">Womens</option>
+          <option value="kids">Kids</option>
+          <option value="unisex">Unisex</option>
+        </select>
+        </div>
+      </div>
+
+      <div className='mt-8 flex flex-wrap justify-center xl:justify-start gap-8 relative'>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+      </div>
+      <div className='relative text-center'>
+        <button className='mt-12 mx-auto w-min text-nowrap  text-white text-xl  bg-[#000] rounded-lg  px-4 py-2 border'>Show more</button>
+      </div>
+    </div>
+   </div>
+</div>
   )
 }
